22834e9fc8903a4d244b55ea4e5be7a8fa6c9fb7
Uwe Fuchs <info@uwefuchs.com>
2018-08-30 10:37:31 +0200

    fake-users added to users-service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { User } from "../model/user";
+import { USERS } from "./mock-users";
 
 @Injectable()
 export class UserService {
 
   constructor() { }
 
+  getUsers(): Observable<User[]> {
+    return of(USERS);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return of(USERS.find(user => user.id === id));
+  }
+
+  // createUser(user: User) {
+  //   return this.http.post(this.baseUrl, user);
+  // }
+
+  // updateUser(user: User) {
+  //   return this.http.put(this.baseUrl + '/' + user.id, user);
+  // }
+
+  // deleteUser(id: number) {
+  //   return this.http.delete(this.baseUrl + '/' + id);
+  // }
 }
