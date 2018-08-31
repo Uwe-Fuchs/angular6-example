f3a2c49c5297b03455c6bcaf3044aa35d5c5afca
Uwe Fuchs <info@uwefuchs.com>
2018-08-31 10:35:54 +0200

    implemented userservice with mocked users

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -6,26 +6,34 @@ import { USERS } from "./mock-users";
 
 @Injectable()
 export class UserService {
+  private baseUrl: string = 'http://localhost:8080/user-portal/users';
 
   constructor() { }
 
   getUsers(): Observable<User[]> {
     return of(USERS);
+    // return this.http.get<User[]>(this.baseUrl);
   }
 
   getUserById(id: number): Observable<User> {
     return of(USERS.find(user => user.id === id));
+    // return this.http.get<User>(this.baseUrl + '/' + id);
   }
 
-  // createUser(user: User) {
-  //   return this.http.post(this.baseUrl, user);
-  // }
+  createUser(user: User): Observable<User> {
+    USERS.push(user);
+    return of(user);
+    // return this.http.post(this.baseUrl, user);
+  }
 
-  // updateUser(user: User) {
-  //   return this.http.put(this.baseUrl + '/' + user.id, user);
-  // }
+  updateUser(user: User): Observable<any> {
+    return of(user);
+    // return this.http.put(this.baseUrl + '/' + user.id, user);
+  }
 
-  // deleteUser(id: number) {
-  //   return this.http.delete(this.baseUrl + '/' + id);
-  // }
+  deleteUser(id: number): Observable<any> {
+    USERS.filter(user => user.id !== id);
+    return of(null as User);
+    // return this.http.delete(this.baseUrl + '/' + id);
+  }
 }
