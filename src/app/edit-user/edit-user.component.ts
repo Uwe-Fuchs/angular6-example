4f24bf065f4434c7840c74fb8a38b34f0c52ba7f
Uwe Fuchs <info@uwefuchs.com>
2018-09-04 10:35:30 +0200

    edit-user implemented (draft)

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+
+import { User } from "../model/user";
+import { UserService } from "../service/user.service";
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  editForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder,private router: Router, private userService: UserService) { 
   }
+
+  ngOnInit(): void {
+
+    let userId = localStorage.getItem("editUserId");
+
+    if(!userId) {
+      alert("Invalid action.")
+      this.router.navigate(['list-user']);
+      return;
+    }
+
+    this.editForm = this.formBuilder.group({
+      id: [],
+      email: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
+
+    this.userService.getUserById(+userId)
+      .subscribe( data => {
+        this.editForm.setValue(data);
+    });
+  }
+
+  onSubmit() {
+
+    this.userService.updateUser(this.editForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['list-user']);
+        },
+        error => {
+          alert(error);
+        });
+    }
 
 }
